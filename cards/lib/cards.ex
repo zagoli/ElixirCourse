c625736979269354950871368271a4561daf03a1
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-02-12 11:29:28 +0100

    documentation and tests

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,7 +1,13 @@
 # iex -S mix
 
 defmodule Cards do
+    @moduledoc """
+    Provides methods for creating and handling a deck of cards
+    """
 
+    @doc """
+    Returns a list of strings representing a deck of poker cards
+    """
     def create_deck do
         values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
         suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -15,10 +21,25 @@ defmodule Cards do
         Enum.shuffle(deck)
     end
 
+    @doc """
+    Determines whether a deck contains a given card
+    ## Examples
+        iex> deck = Cards.create_deck()
+        iex> Cards.contains?(deck, "Ace of Spades")
+        true
+    """
     def contains?(deck, hand) do
         Enum.member?(deck, hand)
     end
 
+    @doc """
+    Divides a deck into a hand with size `hand_size` and the remainder of the deck.
+    ## Examples
+        iex> deck = Cards.create_deck()
+        iex> {hand, _deck} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+    """
     def deal(deck, hand_size) do
         Enum.split(deck, hand_size)
     end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,7 +2,9 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 52 cards" do
+    deck_length = length(Cards.create_deck())
+    assert deck_length == 52
   end
+
 end
