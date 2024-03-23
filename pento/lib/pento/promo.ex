8c417a995c4cd7de09acef8e0bc03f827fe03035
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-03-23 14:51:45 +0100

    upload images in products

---
+++ pento/lib/pento/promo.ex
@@ -0,0 +1,19 @@
+defmodule Pento.Promo do
+  alias Pento.Promo.Recipient
+
+  def change_recipient(%Recipient{} = recipient, attrs \\ %{}) do
+    Recipient.changeset(recipient, attrs)
+  end
+
+  def send_promo(%Recipient{} = recipient, attrs \\ %{}) do
+    # send the email
+    changeset = Recipient.changeset(recipient, attrs)
+
+    if changeset.valid? do
+	    {:ok, changeset}
+	    # send the email
+    else
+	    {:error, changeset}
+    end
+  end
+end
---
+++ pento/lib/pento_web/live/promo_live/promo_live.ex
@@ -0,0 +1,62 @@
+defmodule PentoWeb.PromoLive do
+  use PentoWeb, :live_view
+  alias Pento.Promo
+  alias Pento.Promo.Recipient
+
+  def mount(_params, _session, socket) do
+    {:ok,
+     socket
+     |> assign_recipient()
+     |> clear_form()}
+  end
+
+  def assign_recipient(socket) do
+    socket
+    |> assign(:recipient, %Recipient{})
+  end
+
+  def clear_form(socket) do
+    form =
+      socket.assigns.recipient
+      |> Promo.change_recipient()
+      |> to_form()
+
+    assign(socket, :form, form)
+  end
+
+  def assign_form(socket, changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  def handle_event(
+        "validate",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    changeset =
+      recipient
+      |> Promo.change_recipient(recipient_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_form(socket, changeset)}
+  end
+
+  def handle_event(
+        "save",
+        %{"recipient" => recipient_params},
+        %{assigns: %{recipient: recipient}} = socket
+      ) do
+    case Promo.send_promo(recipient, recipient_params) do
+      {:ok, _changeset} ->
+	      {:noreply,
+	        socket
+	        |> put_flash(:info, "Promo sent!")
+	        |> clear_form()}
+      {:error, changeset} ->
+	      {:noreply,
+	        socket
+	        |> put_flash(:error, "Failed to send promo")
+	        |> assign_form(changeset)}
+    end
+  end
+end
