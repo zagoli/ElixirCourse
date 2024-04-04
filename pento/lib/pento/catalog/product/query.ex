567a72e935f19a1a99fa224accba8afa111f06db
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-04-04 10:27:39 +0200

    product with avg ratings query

--- pento/lib/pento/catalog/product/query.ex
+++ pento/lib/pento/catalog/product/query.ex
@@ -1,22 +1,39 @@
 defmodule Pento.Catalog.Product.Query do
   import Ecto.Query
   alias Pento.Catalog.Product
-  alias Pento.Survey.Rating
+  alias Pento.Survey.{Rating, Demographic}
+  alias Pento.Accounts.User
 
   def base() do
-	  Product
+    Product
   end
 
   def with_user_ratings(user) do
-	  base()
-	  |> preload_user_ratings(user)
+    base()
+    |> preload_user_ratings(user)
   end
 
   def preload_user_ratings(query, user) do
-	  ratings_query = Rating.Query.preload_user(user)
+    ratings_query = Rating.Query.preload_user(user)
 
-	  query
-	  |> preload(ratings: ^ratings_query)
+    query
+    |> preload(ratings: ^ratings_query)
   end
 
+  def with_average_ratings(query \\ base()) do
+    query
+    |> join_ratings
+    |> average_ratings
+  end
+
+  defp join_ratings(query) do
+    query
+    |> join(:inner, [p], r in Rating, on: r.product_id == p.id)
+  end
+
+  defp average_ratings(query) do
+    query
+    |> group_by([p], p.id)
+    |> select([p, r], {p.name, fragment("?::float", avg(r.stars))})
+  end
 end
