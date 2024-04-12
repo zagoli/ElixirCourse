0826513ec6bb508ba2a8c7a03506e9794907de82
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-04-12 17:22:06 +0200

    live view integration tests

--- pento/mix.exs
+++ pento/mix.exs
@@ -60,7 +60,7 @@ defmodule Pento.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:contex, "~> 0.4.0"}
+      {:contex, "~> 0.5.0"}
     ]
   end
 
