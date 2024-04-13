cdcac0837fa41b41f9ce6670e7e29280165e0d67
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-04-13 15:00:32 +0200

    unit test when a rating is present, rating live index do not display form but shows stars

--- pento/test/pento_web/live/rating_live_test.exs
+++ pento/test/pento_web/live/rating_live_test.exs
@@ -3,6 +3,7 @@ defmodule PentoWeb.RatingLiveTest do
   import Phoenix.LiveViewTest
   import Pento.CatalogFixtures
   import Pento.AccountsFixtures
+  import Pento.SurveyFixtures
   alias PentoWeb.RatingLive
 
   defp create_user(_) do
@@ -11,23 +12,51 @@ defmodule PentoWeb.RatingLiveTest do
   end
 
   defp create_product_with_no_ratings(_) do
-    product = product_fixture() |> Map.put(:ratings, [])
+    product =
+      product_fixture()
+      |> Map.put(:ratings, [])
+
     %{product: product}
   end
 
+  defp create_rating(product, user) do
+    rating_fixture(%{user_id: user.id, product_id: product.id})
+  end
+
+  defp create_product_with_rating(rating, product_no_rating) do
+    product_no_rating
+    |> Map.put(:ratings, [rating])
+  end
+
   describe "RatingLive.Index" do
-    setup [:create_product_with_no_ratings, :create_user]
+    setup [:create_user, :create_product_with_no_ratings]
 
     test "when no rating is present, the rating form is rendered", %{
-      product: product,
-      user: current_user
+      user: current_user,
+      product: product
     } do
-      products = [product]
-
       assert render_component(&RatingLive.Index.product_list/1,
-               products: products,
+               products: [product],
                current_user: current_user
              ) =~ "rating-form-#{product.id}"
+    end
+
+    test "when there is a rating, rating details are rendered", %{
+      user: current_user,
+      product: product
+    } do
+      product =
+        create_rating(product, current_user)
+        |> create_product_with_rating(product)
+
+      html =
+        render_component(&RatingLive.Index.product_list/1,
+          products: [product],
+          current_user: current_user
+        )
+
+      refute html =~ "rating-form-#{product.id}"
+      assert html =~ "&#x2605;"
     end
   end
 end
