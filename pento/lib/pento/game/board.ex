de59f67efa7bc2b742971ea71e382b97dfbabf6d
Jacopo <33716357+zagoli@users.noreply.github.com>
2024-04-22 09:51:24 +0200

    game boundary and legality checks

--- pento/lib/pento/game/board.ex
+++ pento/lib/pento/game/board.ex
@@ -39,8 +39,62 @@ defmodule Pento.Game.Board do
   end
 
   def active?(board, shape_name) when is_binary(shape_name) do
-	  active?(board, String.to_existing_atom(shape_name))
+    active?(board, String.to_existing_atom(shape_name))
   end
+
   def active?(%{active_pento: %{name: shape_name}}, shape_name), do: true
   def active?(_board, _shape_name), do: false
+
+  def pick(board, :board), do: board
+
+  def pick(%{active_pento: pento} = board, name) when not is_nil(pento) do
+    if pento.name == name do
+      Map.put(board, :active_pento, nil)
+    else
+      board
+    end
+  end
+
+  def pick(board, shape_name) do
+	  active =
+	    board.completed_pentos
+	    |> Enum.find(&(&1.name == shape_name))
+	    |> Kernel.||(new_pento(board, shape_name))
+
+	  completed = Enum.filter(board.completed_pentos, &(&1.name != shape_name))
+	  %{board | active_pento: active, completed_pentos: completed}
+  end
+
+  defp new_pento(board, shape_name) do
+	  Pentomino.new(name: shape_name, location: midpoints(board))
+  end
+
+  defp midpoints(board) do
+	  {xs, ys} = Enum.unzip(board.points)
+	  {midpoint(xs), midpoint(ys)}
+  end
+
+  defp midpoint(i), do: round(Enum.max(i) / 2.0)
+
+  def drop(%{active_pento: nil} = board), do: board
+  def drop(%{active_pento: pento} = board) do
+	  board
+	  |> Map.put(:active_pento, nil)
+	  |> Map.put(:completed_pentos, [pento | board.completed_pentos])
+  end
+
+  def legal_drop?(%{active_pento: nil}), do: false
+  def legal_drop?(%{active_pento: pento, points: board_points} = board) do
+	  points_on_board =
+	    Pentomino.to_shape(pento).points
+	    |> Enum.all?(fn point -> point in board_points end)
+
+	  no_overlapping_pentos = !Enum.any?(board.completed_pentos, &Pentomino.overlapping?(pento, &1))
+
+	  points_on_board and no_overlapping_pentos
+  end
+
+  def legal_move?(%{active_pento: pento, points: points}) do
+	  pento.location in points
+  end
 end
